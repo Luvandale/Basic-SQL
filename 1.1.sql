fbdc68ce31f2c5a5ab3a5d313a9908420468b103
Luvandale <luvandalediana@gmail.com>
2024-07-29 14:57:38 +0300

    Updated 1.1.sql

--- 1.1.sql
+++ 1.1.sql
@@ -1,3 +1,8 @@
+-- You’ve been asked to extract the data on products from the Product table where there exists a product subcategory. And also include the name of the ProductSubcategory.
+
+-- Columns needed: ProductId, Name, ProductNumber, size, color, ProductSubcategoryId, Subcategory name.
+-- Order results by SubCategory name.
+
 SELECT 
 p.ProductID,
 p.Name,
